957b168b2257f49360531d197104e20a0a0a4a1e
tim <tim.yellow@gmail.com>
2016-05-14 12:40:52 +0800

    firist commit

---
+++ api/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+
+"""
+"100g_calories": 337.7443,
+"100g_carbs": 69.1678,
+"100g_fat": 2.565,
+"100g_protein": 14.118,
+"100g_water": 12.63,
+"all_name": "小麥,",
+"category": "穀物類",
+"id": 0,
+"name": "小麥",
+"piece_weight": 0,
+"trivial": "",
+"unit_calories": 0,
+"unit_carbs": 0,
+"unit_fat": 0,
+"unit_protein": 0,
+"unit_water": 0
+"""
+
+
+class Nutrition(models.Model):
+    calories_100g = models.FloatField(default=0)
+    carbs_100g = models.FloatField(default=0)
+    fat_100g = models.FloatField(default=0)
+    protein_100g = models.FloatField(default=0)
+    water_100g = models.FloatField(default=0)
+    all_name = models.CharField(default=0)
+    category = models.CharField(default=0)
+    id = models.FloatField(default=0)
+    name = models.CharField(default=0)
+    piece_weight = models.FloatField(default=0)
+    trivial = models.FloatField(default=0)
+    unit_calories = models.FloatField(default=0)
+    unit_carbs = models.FloatField(default=0)
+    unit_fat = models.FloatField(default=0)
+    unit_protein = models.FloatField(default=0)
+    unit_water = models.FloatField(default=0)
