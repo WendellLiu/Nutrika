1c8447520613d62e6ea5040c04e35973fee4b320
Ins Huang <ins.huang@gmail.com>
2016-05-14 17:13:20 +0800

    Save the master

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from api.models import Nutrition
+from models import Nutrition
 
 admin.site.register(Nutrition)
--- fda/settings.py
+++ fda/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
-    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
