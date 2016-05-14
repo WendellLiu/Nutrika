e08fbbfdbd315396db73c36c6b562ada639d9075
chairco <chairco@gmail.com>
2016-05-14 18:15:37 +0800

    rm migrations

--- fda/settings.py
+++ fda/settings.py
@@ -76,12 +76,25 @@ WSGI_APPLICATION = 'fda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'test',
+        'NAME': 'mydatabase',
+        'TEST': {
+            'NAME': 'test',
+        },
+    },
+}
+'''
 
 
 # Internationalization
